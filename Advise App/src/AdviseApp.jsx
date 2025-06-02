677eb5204f6b2ebadabee4a519c1f575b00cf313
sivakumarrc32 <sivakumarrconline@gmail.com>
2025-06-02 14:04:51 +0530

    Advice App Project

---
+++ Advise App/src/AdviseApp.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+function AdviseApp() {
+
+    const [advice,setAdvice]=useState("Please Click the Button to Get Advice");
+    const [count,setCount]=useState(0);
+
+    async function getAdvice(){
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const data = await res.json();
+        setAdvice(data.slip.advice);
+        setCount((c)=> c+1);
+    }
+    
+    return (
+        <>
+        <div className='frame container-sm col-sm-6'>
+            <img src="image/advice.jpg" alt=""  className='img'/>
+            <h3>{advice}</h3>
+            <button className='advise'onClick={getAdvice}>Get Advice</button>
+            <p><span>{count}</span> Advice you have Readed</p>
+
+            <p>&copy;Designed By Sivakumar</p>
+        </div>
+        </>
+  )
+}
+
+export default AdviseApp
---
+++ Advise App/src/App.jsx
@@ -0,0 +1,13 @@
+import AdviseApp from './AdviseApp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AdviseApp />
+    </>
+  )
+}
+
+export default App
